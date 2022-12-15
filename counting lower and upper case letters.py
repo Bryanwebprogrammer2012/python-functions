76fbdb5cda185bdb3b057dc73dc090855026446d
Bryanwebprogrammer2012 <119443381+Bryanwebprogrammer2012@users.noreply.github.com>
2022-12-15 16:51:47 +0300

    Add files via upload

---
+++ counting lower and upper case letters.py
@@ -0,0 +1,19 @@
+# counting lower and upper case letters
+
+def count(letters):
+    upper = 0
+    lower = 0
+    for char in letters:
+        if char.isupper():
+            upper = upper +1
+        if char.islower():
+            lower = lower +1
+    print("The number of lower letters is", lower)
+    print("The number of upper letters is", upper)
+    # returning the function    
+    return count
+
+
+add = input("Enter a sentence: ")
+
+print(count(add))
